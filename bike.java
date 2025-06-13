2c96651d2c25c268a0e8d297de7f1c9d9f980543
Harith Bennet <82492613+rithbennet@users.noreply.github.com>
2025-06-13 23:19:19 +0800

    I added a bike!

---
+++ bike.java
@@ -0,0 +1,13 @@
+public class bike {
+    public static void main(String[] args) {
+        System.out.println("Hello, Bike!");
+    }
+
+    public void ride() {
+        System.out.println("Riding the bike!");
+    }
+
+    public void stop() {
+        System.out.println("Stopping the bike!");
+    }
+}
