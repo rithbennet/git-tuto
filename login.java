7e9800de30f6d7700946ea0ad349c4ab14f649cf
Harith Bennet <82492613+rithbennet@users.noreply.github.com>
2025-06-13 23:43:18 +0800

    Feature: Login text

---
+++ login.java
@@ -0,0 +1,13 @@
+public class login {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the login system!");
+    }
+
+    public void authenticate() {
+        System.out.println("Authenticating user...");
+    }
+
+    public void logout() {
+        System.out.println("Logging out...");
+    }
+}
